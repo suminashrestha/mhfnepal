6ad99f360f0375ca028fc5ececc2f94a9a806f18
suminashrestha <suminastha77@gmail.com>
2025-05-14 13:58:12 +0545

    images

--- frontend/src/routes/ContactUs.tsx
+++ frontend/src/routes/ContactUs.tsx
@@ -1,11 +1,14 @@
-
 const ContactUs = () => {
   return (
     <div className="mx-4 md:mx-8 lg:mx-16 xl:mx-32 2xl:mx-64 my-2">
       {/* mobile */}
       <div className="md:hidden">mobile</div>
       {/* desktop */}
-      <div className="hidden md:flex">desktop</div>
+      <div className="hidden md:flex">
+        <div className="">
+          <img src="\public\media\contactUs.jpg" alt="ContactUs" />
+        </div>
+      </div>
     </div>
   );
 };
