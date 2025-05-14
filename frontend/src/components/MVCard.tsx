d05c887b9c04756212bb63bb88db21171eeb3123
suminashrestha <suminastha77@gmail.com>
2025-05-14 19:34:57 +0545

    About us page

--- frontend/src/components/MVCard.tsx
+++ frontend/src/components/MVCard.tsx
@@ -1,12 +1,16 @@
-
-
-const MVCard = ({ title, children}: {title:string, children:React.ReactNode}) => {
+const MVCard = ({
+  title,
+  children,
+}: {
+  title: string;
+  children: React.ReactNode;
+}) => {
   return (
-    <div className="flex flex-col bg-orange-300 w-[90%] md:w-[45%] rounded-lg shadow-md p-2 h-[400px] p-3">
-        <h1 className="text-center">{title}</h1>
-        <p>{children}</p>
-     </div>
-  )
-}
+    <div className="flex flex-col bg-stone-100 w-[90%] md:w-[45%] md:h-[400px] rounded-lg shadow-md justify-center h-[300px] p-4 gap-5 overflow-auto">
+      <h1 className=" text-4xl  font-bold p-2 ">{title}</h1>
+      <p className="text-xl/loose  ">{children}</p>
+    </div>
+  );
+};
 
-export default MVCard
+export default MVCard;
--- frontend/src/routes/About.tsx
+++ frontend/src/routes/About.tsx
@@ -2,110 +2,93 @@ import MVCard from "@/components/MVCard";
 
 const About = () => {
   return (
-    <div className="p-2 md:p-8 lg:p-16 flex flex-col gap-5 items-center">
+    <div className="px-8 md:px-8 lg:px-16 flex flex-col gap-5 items-center ">
+      <div className="w-screen bg-green-700 text-white text-center py-16">
+        <h1 className="text-4xl font-bold">MAHABIR HEALTH FOUNDATION</h1>
+      </div>
+
       {/* introdiv */}
-      <div className="bg-red-500 flex flex-col p-4 rounded-md w-full">
-        <h1>Introduction</h1>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis
-          voluptatum repudiandae non. Ducimus modi accusantium quod dolore
-          molestiae vero quaerat dignissimos, natus maiores voluptates corrupti
-          repellat, consectetur accusamus ullam, ipsam pariatur! Tempora
-          perspiciatis non delectus corporis, quam nesciunt voluptate a debitis
-          nihil iusto repellat tempore eveniet libero sapiente quis accusamus
-          dignissimos temporibus corrupti. Saepe aut corporis hic eveniet
-          ducimus sunt, laborum sapiente exercitationem harum magni voluptates
-          debitis, modi veritatis quis quasi. Voluptate blanditiis vero
-          similique cumque, rem libero fugit aliquam, sequi tempore officia
-          nesciunt aperiam culpa reiciendis quam! Officiis eligendi sunt error
-          numquam consequatur molestias saepe placeat eos similique quod quam
-          nihil a, qui, vitae eaque inventore animi perspiciatis ea quae ullam
-          excepturi. Possimus natus provident, eveniet fugit amet iure
-          recusandae voluptates tenetur saepe corrupti perferendis! Doloremque
-          provident maiores voluptas earum illo suscipit cum aliquid quaerat at?
-          Deserunt alias nemo explicabo veritatis autem, culpa ad nam voluptatum
-          aperiam architecto ullam corporis possimus aut. Fugit culpa amet iste
-          placeat odio facilis minima obcaecati repudiandae voluptate nisi,
-          omnis, ullam, neque distinctio nemo temporibus! Porro voluptatum
-          repellat veritatis est dignissimos tenetur odit, quia alias aperiam at
-          aut dolorum numquam nostrum, reiciendis illum facilis sit corporis
-          unde tempora magnam inventore. Et eaque est, perspiciatis cumque optio
-          ea nihil possimus consectetur. Nam in temporibus adipisci nulla
-          aliquid molestias molestiae nisi, doloribus possimus dolore? Cum,
-          asperiores accusantium! Error dolore perferendis debitis ad delectus,
-          expedita sint nemo suscipit laboriosam alias minima similique eveniet,
-          quae et voluptatem cum ullam corporis ipsum harum, deleniti eos
-          pariatur qui laborum aspernatur! Magnam odit nesciunt provident,
-          perspiciatis molestiae cupiditate architecto eos aperiam ipsum
-          obcaecati dolore amet commodi vero eius blanditiis sequi aut id. Quam
-          enim voluptatibus maxime porro quo quae quis temporibus sit facere,
-          quos, placeat praesentium? Iusto culpa pariatur a repudiandae! Beatae,
-          porro modi deserunt officia ipsum quasi minima ab. Rerum omnis a odio
-          reiciendis dolores recusandae sint enim velit sunt dignissimos
-          assumenda pariatur expedita laborum ratione autem excepturi, sequi in
-          culpa perferendis impedit hic aspernatur accusamus ab! Facere dolorem
-          ex deserunt omnis velit ea sit dolor voluptatum ab distinctio possimus
-          quibusdam labore modi corrupti fuga qui voluptatem, nemo blanditiis
-          corporis dolorum voluptas similique nostrum in esse. Aliquam sunt
-          totam earum atque quo? Dolorum praesentium quos atque libero
-          distinctio molestias expedita ea dignissimos explicabo cupiditate
-          blanditiis temporibus recusandae vitae accusantium reprehenderit
-          provident, voluptatem ullam ipsum voluptate optio modi tempora est
-          asperiores aliquid. Non alias incidunt magnam iure, qui voluptates
-          sint fugiat dicta est modi laborum nemo eligendi facilis nostrum iusto
-          repellendus consectetur aliquam et dolorum id error? Nesciunt sunt
-          consequuntur eius, maiores doloribus, repellat tempora eos aliquid
-          illo sequi esse quia iste possimus totam vero mollitia quod qui
-          voluptatum laboriosam officia magni voluptatem provident ad nobis?
-          Beatae amet, exercitationem harum obcaecati fuga eius distinctio
-          quisquam reprehenderit aliquid earum perspiciatis adipisci, nam ullam
-          minus incidunt dicta est ut maiores molestias, odio cumque maxime
-          laboriosam! Delectus sequi unde ratione in eaque quo itaque, ab nihil
-          eos amet nesciunt sed est quos quasi aliquid necessitatibus sunt
-          inventore quas? Culpa similique amet quis quae placeat.
+      <div className="flex flex-col gap-2 p-4 rounded-md md:w-2/3">
+        <h1 className="text-4xl  font-bold p-3">INTRODUCTION</h1>
+        <p className="text-xl/loose p-2 ">
+          Mahabir Health Foundations was established with the aim of Building a
+          Healthier Community through the provision of health services at the
+          doorstep of community. It strives for changing the health-service
+          seeking behaviour of target community that leads them early prevention
+          and reduction in the cost of treatment. Further, we promote healthy
+          and sustainable communities by providing health & social counseling,
+          health camps, medicine distribution, awareness of hygiene maintain,
+          sanitation, mental health development.
+        </p>
+        <p className="text-xl/loose p-px m-2 ">
+          MHF has started health programmes based on community, home, college,
+          schools and corporate houses where health awareness on preventive
+          health check up and healthy lifestyles is focused. For this, the
+          organisation has been backed by the interdisciplinary personalities
+          who have experiences of different sectors and the hunger for creating
+          healthy community.
+        </p>
+        <p className="text-xl/loose p-px m-2 ">
+          We conducted 126 health camps followed by health awareness sessions in
+          the vicinities of Kathmandu Valley. In addition, blood donations in
+          different places have been organized. Currently we have focused on:
+          <li>Integrated Health Academy</li>
+          <li> Community Health Awareness Programme and</li>
+          <li>
+            Employment generation. As a non-profit social development
+            organisation, we invite all dignitaries, professionals and social
+            activities to be a part of it.
+          </li>
         </p>
       </div>
 
       {/* teamdiv */}
-      <div className="p-4">
-        <h1 className="text-center">Teams</h1>
+      <div className="p-4 ">
+        <h1 className="text-4xl  font-bold text-center p-4">TEAMS</h1>
         <img src="team.jpeg" alt="teamImage" className="rounded-md shadow-md" />
       </div>
 
-      <div className="flex flex-col gap-8 md:flex-row items-center justify-between p-5">
-        <MVCard title="Mission">
+      <div className="flex flex-col gap-8 md:flex-row items-center justify-between p-3 md:w-2/3">
+        <MVCard title="MISSION">
           {/* content */}
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
-          adipisci sunt veritatis ut illo nobis repellendus beatae magnam neque
-          unde ipsam consequuntur nostrum dolores, odio nemo sit exercitationem
-          ea necessitatibus officiis, quos aspernatur temporibus? Nobis quam
-          laboriosam ipsa quis quidem rerum debitis voluptatum totam tenetur.
-          Illo quisquam cupiditate assumenda ratione!
+          Our mission is to promote healthy and sustainable communities by
+          providing resources, strengthening collaborative relationships and
+          supporting initiatives.
         </MVCard>
-        <MVCard title="Vision">
+        <MVCard title="VISION">
           {/* content */}
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta
-          adipisci sunt veritatis ut illo nobis repellendus beatae magnam neque
-          unde ipsam consequuntur nostrum dolores, odio nemo sit exercitationem
-          ea necessitatibus officiis, quos aspernatur temporibus? Nobis quam
-          laboriosam ipsa quis quidem rerum debitis voluptatum totam tenetur.
-          Illo quisquam cupiditate assumenda ratione!
+          Healthy Practices, People and Places. Our vision is healthy people
+          with healthy practice in healthy communities that include spiritual,
+          emotional, physicial, social and economic well- being.
         </MVCard>
       </div>
 
       {/* values */}
 
-      <div className="w-[80%] p-3 bg-orange-300 rounded-md shadow-md">
-        <h1 className="text-center font-bold">Values</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores amet
-          earum repudiandae ratione dolores officia voluptates ullam officiis
-          quas magni non illo exercitationem repellat aspernatur modi
-          repellendus facilis illum in, omnis commodi ad dignissimos qui
-          consectetur. Ab exercitationem velit cupiditate eligendi laboriosam
-          omnis facere quisquam, et, maxime necessitatibus eveniet eius
-          quibusdam porro labore cum eaque at suscipit iste esse magni!
-        </p>
+      <div className="w-[80%] p-3 bg-stone-100 rounded-md shadow-md md:w-2/3 text-xl/loose">
+        <h1 className="text-center font-bold text-4xl p-2">VALUES</h1>
+        <ul className="list-disc p-5">
+          <li> We Value our connectivity to the public health community.</li>
+          <li> We value innovation and idea driven solutions.</li>
+          <li> We value being a high impact organization.</li>
+          <li>
+            Foster an environment of positive change through innovation,
+            creativity & continuous improvement.
+          </li>
+          <li>
+            Empathy and compassion are fundamental values and are reflected in
+            our relationships with others.
+          </li>
+          <li>
+            Accountability is a value demonstrated through careful stewardship,
+            accounting and monitoring practices.
+          </li>
+          <li>
+            We focus on adopting. Practicing and promoting principles of good
+            governance in all our work that will enhance accountability,
+            transparency and quality of response to those we serve.
+          </li>
+        </ul>
       </div>
     </div>
   );
